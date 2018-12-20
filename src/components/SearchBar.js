9fe643c511c980da845a761360858449420f7e24
Duy <duy@duys-mbp.home>
2018-12-20 00:20:10 -0800

    state for ticker

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            ticker: ''
+        };
+    }
+
+    onInputChange = (e) => {
+        this.setState({
+            ticker: e.target.value
+        });
+    }
+
     render () {
         return (
-            <div>searchbar</div>
+            <div>
+                <label>Enter stock ticker</label>
+                <input 
+                    type="text"
+                    value={this.state.ticker}
+                    onChange={this.onInputChange} />
+            </div>
         );
     }
 }
