605f53002a91c9ca491af03eb6a70e73441f0ac7
Duy <duy@duys-mbp.home>
2018-12-20 01:12:59 -0800

    Add quote detail

---
+++ src/components/QuoteDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class QuoteDetail extends React.Component {
+
+    render() {
+        return (
+            <div>QuoteDetail</div>
+        );
+    }
+}
