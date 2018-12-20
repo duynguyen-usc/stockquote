4d6f72df3047ff4524369f29778a09c3a6b467d3
Duy <duy@duys-mbp.home>
2018-12-20 01:52:44 -0800

    getQuote from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import alphavantage from '../apis/alphavantage';
 
 export default class App extends React.Component {
+
+    getQuote = async () => {
+        const quote = await alphavantage.get('query', {
+            params: { 
+                symbol: "SPX"
+            }
+        });
+        
+        console.log(quote);
+    }
+
+    componentDidMount() {
+        this.getQuote()
+    }
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar/>
+                <div></div>
             </div>
         );
     }
