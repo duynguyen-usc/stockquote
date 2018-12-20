f73ddf414c72a58efc27992597f277af012a327a
Duy <duy@duys-mbp.home>
2018-12-20 00:28:57 -0800

    styling for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,12 +17,16 @@ export default class SearchBar extends React.Component {
 
     render () {
         return (
-            <div>
-                <label>Enter stock ticker</label>
-                <input 
-                    type="text"
-                    value={this.state.ticker}
-                    onChange={this.onInputChange} />
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Enter stock ticker</label>
+                        <input 
+                            type="text"
+                            value={this.state.ticker}
+                            onChange={this.onInputChange} />
+                    </div>
+                </form>
             </div>
         );
     }
